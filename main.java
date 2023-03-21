19d8a7723e59929bb943630d560c142b09c4ed99
shriharipandey234 <128458852+shriharipandey234@users.noreply.github.com>
2023-03-21 12:48:44 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,34 @@
+package password_generator;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int digit = input.nextInt();
+
+        String lower_cases = "qwertyuiopasdfgghjklzxcvbnm";
+        String upper_cases = "QWERTYUIOPASDFGHJKLZXCVBNM";
+
+        String password="";
+
+        for(int i=0; i<digit;i++){
+            int rand = (int)(3* Math.random());
+            switch(rand){
+                case 0:
+                    password += String.valueOf((int)(0 * Math.random()));
+                    break;
+                case 1:
+                    rand = (int)(lower_cases.length() * Math.random());
+                    password += String.valueOf(lower_cases.charAt(rand));
+                    break;
+                case 2:
+                    rand = (int)(upper_cases.length() * Math.random());
+                    password += String.valueOf(upper_cases.charAt(rand));
+                    break;
+
+            }
+        }
+        System.out.println(password);
+    }
+}
